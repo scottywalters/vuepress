88dc9dbc6b5ab7930f2111117bb5cbc9fd496e50
Scotty Walters <scottycwalters@gmail.com>
2020-09-13 14:03:01 -0400

    Remove locales

--- packages/docs/docs/.vuepress/config.js
+++ packages/docs/docs/.vuepress/config.js
@@ -8,11 +8,11 @@ module.exports = ctx => ({
       title: 'Physics Design System',
       description: 'Design System for SunCHECK™'
     },
-    '/zh/': {
-      lang: 'zh-CN',
-      title: 'Physics Design System',
-      description: 'Design System for SunCHECK™'
-    }
+    // '/zh/': {
+    //   lang: 'zh-CN',
+    //   title: 'Physics Design System',
+    //   description: 'Design System for SunCHECK™'
+    // }
   },
   head: [
     ['link', { rel: 'icon', href: `/logo.png` }],
@@ -51,20 +51,20 @@ module.exports = ctx => ({
           '/theme/': getThemeSidebar('Theme', 'Introduction')
         }
       },
-      '/zh/': {
-        label: '简体中文',
-        selectText: '选择语言',
-        ariaLabel: '选择语言',
-        editLinkText: '在 GitHub 上编辑此页',
-        lastUpdated: '上次更新',
-        nav: require('./nav/zh'),
-        sidebar: {
-          '/zh/api/': getApiSidebar(),
-          '/zh/guide/': getGuideSidebar('指南', '深入'),
-          '/zh/plugin/': getPluginSidebar('插件', '介绍', '官方插件'),
-          '/zh/theme/': getThemeSidebar('主题', '介绍')
-        }
-      }
+      // '/zh/': {
+      //   label: '简体中文',
+      //   selectText: '选择语言',
+      //   ariaLabel: '选择语言',
+      //   editLinkText: '在 GitHub 上编辑此页',
+      //   lastUpdated: '上次更新',
+      //   nav: require('./nav/zh'),
+      //   sidebar: {
+      //     '/zh/api/': getApiSidebar(),
+      //     '/zh/guide/': getGuideSidebar('指南', '深入'),
+      //     '/zh/plugin/': getPluginSidebar('插件', '介绍', '官方插件'),
+      //     '/zh/theme/': getThemeSidebar('主题', '介绍')
+      //   }
+      // }
     }
   },
   plugins: [
