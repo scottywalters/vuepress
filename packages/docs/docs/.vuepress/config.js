74ccd35c810e4adbb3f8fd0254965c7aea85a843
Scotty Walters <scottycwalters@gmail.com>
2020-09-18 12:43:10 -0400

    Fixed: sidebar

--- packages/docs/docs/.vuepress/config.js
+++ packages/docs/docs/.vuepress/config.js
@@ -31,12 +31,13 @@ module.exports = ctx => ({
     editLinks: true,
     docsDir: 'packages/docs/docs',
     // #697 Provided by the official algolia team.
-    algolia: ctx.isProd ? ({
-      apiKey: '3a539aab83105f01761a137c61004d85',
-      indexName: 'vuepress'
-    }) : null,
+    // algolia: ctx.isProd ? ({
+    //   apiKey: '3a539aab83105f01761a137c61004d85',
+    //   indexName: 'vuepress'
+    // }) : null,
     logo: '/logo.png',
     smoothScroll: true,
+    search: true,
     locales: {
       '/': {
         label: 'English',
@@ -47,25 +48,11 @@ module.exports = ctx => ({
         nav: require('./nav/en'),
         sidebar: {
           '/api/': getApiSidebar(),
-          '/guide/': getGuideSidebar('Guide', 'Advanced'),
+          '/guidelines/': getGuideSidebar('Guidelines', 'Advanced'),
           '/plugin/': getPluginSidebar('Plugin', 'Introduction', 'Official Plugins'),
           '/theme/': getThemeSidebar('Theme', 'Introduction')
         }
       }
-      // '/zh/': {
-      //   label: '简体中文',
-      //   selectText: '选择语言',
-      //   ariaLabel: '选择语言',
-      //   editLinkText: '在 GitHub 上编辑此页',
-      //   lastUpdated: '上次更新',
-      //   nav: require('./nav/zh'),
-      //   sidebar: {
-      //     '/zh/api/': getApiSidebar(),
-      //     '/zh/guide/': getGuideSidebar('指南', '深入'),
-      //     '/zh/plugin/': getPluginSidebar('插件', '介绍', '官方插件'),
-      //     '/zh/theme/': getThemeSidebar('主题', '介绍')
-      //   }
-      // }
     }
   },
   plugins: [
