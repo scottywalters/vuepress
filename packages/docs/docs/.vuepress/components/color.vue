7b560134f75a7aacb01d3826cc099ff478c70a51
Scotty Walters <scottycwalters@gmail.com>
2020-10-15 11:25:54 -0400

    wip: Passing data from .md file

--- packages/docs/docs/.vuepress/components/color.vue
+++ packages/docs/docs/.vuepress/components/color.vue
@@ -1,34 +1,48 @@
 <template>
   <div>
-    <b-table  :fields="fields" :items="items" responsive="sm" head-variant="light" >
+    <b-table
+      :fields="fields"
+      :items="this.data.items"
+      responsive="sm"
+      head-variant="light"
+    >
       <template v-slot:cell(example)="data">
-        <b-img rounded="circle" blank :blank-color="data.item.hexValue" width="64" alt="placeholder"></b-img>
+        <b-img
+          rounded="circle"
+          blank
+          :blank-color="data.item.hexValue"
+          width="64"
+          alt="Color"
+          thumbnail
+        ></b-img>
+        <!-- {{ data.item.hexValue }} -->
+        <b-button @click="debug(data.item.hexValue)" v-text="data.item.hexValue"/>
       </template>
     </b-table>
   </div>
 </template>
 
 <script>
-  export default {
+export default {
+  data() {
+    return {
+      fields: ["token", "usage", "hexValue", "example"]
+    };
+  },
+  computed: {
     data() {
-      return {
-        // Note `isActive` is left out and will not appear in the rendered table
-        fields: ['token', 'usage', 'hexValue', 'example'],
-        items: [
-          { token: '$ui-background', usage: 'This is used for the default background', hexValue: '#000000'},
-          { token: '$interactive-01', usage: 'Primary interactive color & Primary buttons', hexValue: '#0f62fe' },
-          { token: '$interactive-02', usage: 'Secondary interactive color & Secondary button', hexValue: '#393939' },
-          { token: '$ui-background', usage: 'This is used for the default background', hexValue: '#000000'},
-          { token: '$interactive-01', usage: 'Primary interactive color & Primary buttons', hexValue: '#0f62fe' },
-          { token: '$interactive-02', usage: 'Secondary interactive color & Secondary button', hexValue: '#393939' }
-        ]
-      }
-    },
-    computed: {
-    data () {
-      return this.$page.frontmatter
-    },
+      return this.$page.frontmatter;
+    }
+  },
+  methods: {
+    debug(event) {
+      console.log('hexValue -', event);
+    }
   }
-  }
+};
 </script>
 
+<style lang="stylus" scoped>
+.img-thumbnail
+  padding: 0
+</style>
--- packages/docs/docs/suncheck/guidelines/color.md
+++ packages/docs/docs/suncheck/guidelines/color.md
@@ -1,15 +1,31 @@
 ---
-
-sidebarDepth: 0
-color: '#ffffff'
-
+{
+  "sidebarDepth": 0,
+  "color": { "white": "#ffffff", "black": "#000000" },
+  items:
+    [
+      {
+        token: "$interactive-01",
+        usage: "Primary interactive color & Primary buttons",
+        hexValue: "#262626",
+      },
+      {
+        token: "$ui-background",
+        usage: "This is used for the default background",
+        hexValue: "#FFFFFF",
+      },
+      {
+        token: "$interactive-02",
+        usage: "Secondary interactive color & Secondary button",
+        hexValue: "#F4F4F4",
+      },
+    ],
+}
 ---
 
 # Color <Badge text="in-progress" type="warning" vertical="middle" /> <Badge text="r3.1.0" type="info" vertical="middle" />
 
-Maintaining consistent user interfaces throughout Sun Nuclear's products, demands exstensive guidance on color usage. 
+Maintaining consistent user interfaces throughout Sun Nuclear's products, demands exstensive guidance on color usage.
 The following include the UI Base colors & the Interactive colors (hover, state, etc.).
 
 <color />
-
-
